656d2238d0b43b17bd0ed10ef2eca2d48a8e751d
schadstoff <schadlucas@gmail.com>
2015-08-18 23:16:32 +0200

    Created Basic Layout
    Colors are awful, sorry.

--- src/tierlist.js
+++ src/tierlist.js
@@ -3,6 +3,12 @@
         .module('smitetierlist', [])
         .controller('IndexController', IndexController);
 
-    function IndexController($scope) {
+    function IndexController($scope, $http) {
+        $scope.gods = [];
+        $http
+            .get('gods.json')
+            .then(function(response){
+                $scope.gods = response.data;
+            })
     }
 })();
