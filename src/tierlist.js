7ad34accb164bdd6c3658849a065379bedc73698
schadstoff <schadlucas@gmail.com>
2015-08-19 00:19:31 +0200

    Added basic drag and drop functionality

--- src/tierlist.js
+++ src/tierlist.js
@@ -18,7 +18,26 @@
             'c': {color: 'grey', gods: []},
             'd': {color: 'grey', gods: []}
         };
-        
+        $scope.drop = drop;
+        $scope.resetGod = resetGod;
+
+
+        function drop(god, event, tier) {
+            $scope.availableGods.splice($scope.availableGods.indexOf(god), 1);
+            $scope.tiers[tier].gods.push(god);
+        }
+
+        function resetGod(god, event) {
+            for (var key in $scope.tiers) if ($scope.tiers.hasOwnProperty(key)) {
+                var index = $scope.tiers[key].gods.indexOf(god);
+                if (index !== -1) {
+                    $scope.tiers[key].gods.splice(index, 1);
+                    $scope.availableGods.push(god);
+                    break;
+                }
+            }
+        }
+
         $http
             .get('gods.json')
             .then(function (response) {
