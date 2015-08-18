e29af769b7f3f330d7435783dea95b5d05d53f22
schadstoff <schadlucas@gmail.com>
2015-08-19 00:04:25 +0200

    Simplified layout creation
    Instead of static HTML the layout will created from an configuration
    object.

--- src/tierlist.js
+++ src/tierlist.js
@@ -5,10 +5,25 @@
 
     function IndexController($scope, $http) {
         $scope.gods = [];
+        $scope.availableGods = [];
+        $scope.tiers = {
+            'ss': {color: 'red', gods: []},
+            'splus': {color: 'blue', gods: []},
+            's': {color: 'blue', gods: []},
+            'sminus': {color: 'blue', gods: []},
+            'aplus': {color: 'green', gods: []},
+            'a': {color: 'green', gods: []},
+            'aminus': {color: 'green', gods: []},
+            'b': {color: 'grey', gods: []},
+            'c': {color: 'grey', gods: []},
+            'd': {color: 'grey', gods: []}
+        };
+        
         $http
             .get('gods.json')
             .then(function (response) {
-                $scope.gods = response.data;
+                $scope.gods = angular.copy(response.data);
+                $scope.availableGods = angular.copy(response.data);
             })
     }
 })();
