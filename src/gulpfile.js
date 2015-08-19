68fdb64dff83df8b4e6fe902c6f7189f3f78e5fc
schadstoff <schadlucas@gmail.com>
2015-08-19 02:23:57 +0200

    Replaced wiredep with gulp-inject

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,15 +1,11 @@
 (function () {
+    'use strict';
+
     var gulp = require('gulp');
-    var wiredep = require('wiredep').stream;
+    var inject = require('gulp-inject');
     var gulpBowerFiles = require('gulp-bower-files');
     var concat = require('gulp-concat');
-
-    gulp.task('wiredep', function () {
-        gulp
-            .src('index.html')
-            .pipe(wiredep())
-            .pipe(gulp.dest('../dist/'));
-    });
+    var series = require('stream-series');
 
     gulp.task('bower', function () {
         gulpBowerFiles().pipe(gulp.dest("../dist/bower_components"));
@@ -22,5 +18,20 @@
             .pipe(gulp.dest('../dist/'))
     });
 
-    gulp.task('default', ['wiredep', 'bower', 'angular']);
+    gulp.task('assets', function () {
+        gulp
+            .src('assets/**/*')
+            .pipe(gulp.dest('../dist/assets/'));
+    });
+
+    gulp.task('wiredep', function () {
+        var vendorStream = gulp.src(['../dist/**/*.js', '!../dist/app.js', '../dist/**/*.css'], {read: false});
+        var appStream = gulp.src('../dist/app.js');
+        gulp
+            .src('index.html')
+            .pipe(inject(series(vendorStream, appStream), {relative: true, ignorePath: '../dist/'}))
+            .pipe(gulp.dest('../dist/'))
+    });
+
+    gulp.task('default', ['bower', 'angular', 'assets', 'wiredep']);
 })();
