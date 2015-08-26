8d587b3b612ff5d4ea7426ab4cb1db69cf5041b3
schadstoff <schadlucas@gmail.com>
2015-08-26 05:42:10 +0200

    Added God Name as title for icons

--- src/modules/tierlist/avatar.directive.js
+++ src/modules/tierlist/avatar.directive.js
@@ -11,7 +11,7 @@
             scope: {
                 god: '=god'
             },
-            template: '<img ng-if="god" class="avatar" id="{{god.name}}" ng-src="{{god.icon}}" ' +
+            template: '<img ng-if="god" class="avatar" id="{{god.name}}" ng-src="{{god.icon}}" title="{{god.name}}"' +
                 'fallback-src="/lib/assets/icons/kappa.jpg" ng-drag="true" ng-drag-data="god">'
         };
     }
