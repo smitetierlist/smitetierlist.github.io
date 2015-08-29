cdb057a2728d48734d3ee24414ef6f83c4f5c04f
schadstoff <schadlucas@gmail.com>
2015-08-29 03:40:05 +0200

    [gulp] Minify & Uglify implemented

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -11,8 +11,13 @@
     var watch = require('gulp-watch');
     var batch = require('gulp-batch');
     var less = require('gulp-less');
+    var minifyJS = require('gulp-uglify');
     var minifyCSS = require('gulp-minify-css');
+    var minifyHTML = require('gulp-minify-html');
     var autoprefixer = require('gulp-autoprefixer');
+    var filter = require('gulp-filter');
+    var merge = require('merge-stream');
+
 
     gulp.task('clean', function (cb) {
         del.sync('../index.html', {force: true});
@@ -21,14 +26,25 @@
     });
 
     gulp.task('bower', ['clean'], function () {
-        return gulpBowerFiles()
-            .pipe(gulp.dest("../lib/"));
+        var js = gulpBowerFiles()
+            .pipe(filter('**/*.js'))
+            .pipe(concat('vendor.js'))
+            .pipe(minifyJS())
+            .pipe(gulp.dest('../lib/'));
+        var css = gulpBowerFiles()
+            .pipe(filter('**/*.css'))
+            .pipe(concat('vendor.css'))
+            .pipe(autoprefixer())
+            .pipe(minifyCSS())
+            .pipe(gulp.dest('../lib/'));
+        return merge(js, css);
     });
 
     gulp.task('angular', ['clean'], function (cb) {
         gulp
             .src(['modules/**/*.module.js', 'modules/**/*.js'])
             .pipe(concat('app.js'))
+            .pipe(minifyJS({mangle:true}))
             .pipe(gulp.dest('../lib/'));
 
         gulp
@@ -47,7 +63,7 @@
             .pipe(less())
             .pipe(minifyCSS())
             .pipe(autoprefixer())
-            .pipe(gulp.dest('../lib/assets/styles/'));
+            .pipe(gulp.dest('../lib/'));
 
         gulp
             .src(['assets/**/*', '!**/*.less'])
@@ -58,7 +74,8 @@
 
     gulp.task('inject', ['bower', 'angular', 'assets'], function () {
         // Inject js
-        var vendorJS = gulp.src(['../lib/**/*.js', '!../lib/app.js'], {read: false});
+        //var vendorJS = gulp.src(['../lib/**/*.js', '!../lib/app.js'], {read: false});
+        var vendorJS = gulp.src('../lib/vendor.js', {read: false});
         var customJS = gulp.src('../lib/app.js', {read: false});
         var vendorCSS = gulp.src(['../lib/**/*.css', '!../lib/assets/styles/**/*.css'], {read: false});
         var customCSS = gulp.src('../lib/assets/styles/**/*.css', {read: false});
@@ -68,6 +85,7 @@
             .pipe(inject(customJS, {relative: true, ignorePath: '../', starttag: '<!-- inject:js:custom -->'}))
             .pipe(inject(vendorCSS, {relative: true, ignorePath: '../', starttag: '<!-- inject:css:vendor -->'}))
             .pipe(inject(customCSS, {relative: true, ignorePath: '../', starttag: '<!-- inject:css:custom -->'}))
+            .pipe(minifyHTML())
             .pipe(gulp.dest('../'));
     });
 
